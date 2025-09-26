a9c690bd5a048e347f4e8bdc41e33ee05fcbf6c4
NewNamesAreHard <oscar.asserlund@gmail.com>
2025-09-26 17:14:38 +0200

    Added compile time definition of backend to secrets_template.h to ensure compilation in CI - To fix compilation error in CI step we need BACKEND_URL to exist as a compile time definition for compiler to expand

--- ControllerUnit/src/arduino_secrets_template.h
+++ ControllerUnit/src/arduino_secrets_template.h
@@ -9,4 +9,10 @@ constexpr char WIFI_PASSWORD[] = "YOUR_WIFI_PASSWORD";
 constexpr char MQTT_BROKER[] = "MQTT_BROKER_IP";
 constexpr unsigned int MQTT_PORT = 1883;
 
+// Backend endpoint to forward enriched messages (HTTP/HTTPS URL)
+// Example: "http://192.168.0.10:8080/ingest" or "https://example.com/ingest"
+#ifndef BACKEND_URL
+#define BACKEND_URL "YOU_BACKEND_URL"
+#endif
+
 #endif // ARDUINO_SECRET_H
