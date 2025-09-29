a72c31a78c009d6e019d91cb5003ce707d3a6977
NewNamesAreHard <oscar.asserlund@gmail.com>
2025-09-29 16:45:15 +0200

    SensorUnit: Remove NTPClient and timestamp logic from main.cpp
    - Deleted NTPClient usage, time functions, and related setup/teardown code.
    - Removed timestamp field from sensor data payload.
    - Updated publish interval to 5000ms and simplified sensor data creation.
    - Cleaned up unused includes and function declarations.
    - platformio.ini: Removed arduino-libraries/NTPClient from dependencies.

--- SensorUnit/src/main.cpp
+++ SensorUnit/src/main.cpp
@@ -1,37 +1,29 @@
 #include <Arduino.h>
 #include <WiFiS3.h>
-#include <time.h>
 #include <ArduinoJson.h>
 #include <PubSubClientQoS2.h>
-#include <NTPClient.h>
 #include "DHT11.h"
 #include "arduino_secrets.h"
 #include "device_info.h"
 
 constexpr bool debugOn = true;
 constexpr int DHT11_PIN = 4;
-constexpr int GMT_OFFSET_PLUS_2 = 7200;
+constexpr unsigned long publishInterval = 5000;
 
 DHT11 dht11(DHT11_PIN);
 WiFiClient client;
 PubSubClient mqttClient(client);
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", GMT_OFFSET_PLUS_2, 60000);
 
-void connectWiFi();
-void callback(char *topic, uint8_t *payload, unsigned int length);
+void setupWiFi();
 void setupMQTTClient();
-void setupNTP();
 void connectMQTT();
-void getTimestamp(char *buffer, size_t len);
-void createSensorData(StaticJsonDocument<128> &doc, float temperature, float humidity, const char *timestamp, const char *deviceID);
+void createSensorData(StaticJsonDocument<128> &doc, float temperature, float humidity, const char *deviceID);
 
 void setup()
 {
     Serial.begin(115200);
     dht11.begin();
-    connectWiFi();
-    setupNTP();
+    setupWiFi();
     initDeviceInfo();
     setupMQTTClient();
 }
@@ -53,16 +45,13 @@ void loop()
     mqttClient.loop();
 
     static unsigned long lastPublish = 0;
-    const unsigned long publishInterval = 15000;
     if (millis() - lastPublish >= publishInterval)
     {
         StaticJsonDocument<128> doc;
-        char timestamp[25];
-        getTimestamp(timestamp, sizeof(timestamp));
         float temperature = dht11.getTemperature();
         float humidity = dht11.getHumidity();
 
-        createSensorData(doc, temperature, humidity, timestamp, getDeviceID());
+        createSensorData(doc, temperature, humidity, getDeviceID());
         char payload[128];
         serializeJsonPretty(doc, payload);
         for (uint8_t qos = QOS0; qos <= QOS2; ++qos)
@@ -130,38 +119,6 @@ void connectWiFi()
     }
 }
 
-void setupNTP()
-{
-    ntpUDP.begin(2390);
-    timeClient.begin();
-    while (!timeClient.update())
-    {
-        delay(100);
-    }
-}
-
-void getTimestamp(char *buffer, size_t len)
-{
-    if (!timeClient.update())
-    {
-        timeClient.forceUpdate();
-    }
-    time_t rawTime = timeClient.getEpochTime();
-    struct tm timeinfo;
-    struct tm *tmptr = gmtime(&rawTime);
-    if (tmptr)
-    {
-        timeinfo = *tmptr;
-    }
-    snprintf(buffer, len, "%04d-%02d-%02dT%02d:%02d:%02dZ",
-             timeinfo.tm_year + 1900,
-             timeinfo.tm_mon + 1,
-             timeinfo.tm_mday,
-             timeinfo.tm_hour,
-             timeinfo.tm_min,
-             timeinfo.tm_sec);
-}
-
 void connectMQTT()
 {
     static unsigned long lastDisconnectTime = 0;
@@ -199,7 +156,7 @@ void connectMQTT()
     }
 }
 
-void createSensorData(StaticJsonDocument<128> &doc, float temperature, float humidity, const char *timestamp, const char *deviceID)
+void createSensorData(StaticJsonDocument<128> &doc, float temperature, float humidity, const char *deviceID)
 {
     doc.clear();
     if (isnan(temperature))
@@ -218,5 +175,4 @@ void createSensorData(StaticJsonDocument<128> &doc, float temperature, float hum
     {
         doc["Humidity"] = humidity;
     }
-    doc["Timestamp"] = timestamp;
 }
